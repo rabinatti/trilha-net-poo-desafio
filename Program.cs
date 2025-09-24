06a0f8b50fb2169beaa8973c8002b85095b7f951
Raphael Binatti Filardi <rabinatti@gmail.com>
2025-09-24 17:58:11 -0300

    Desafio Concluido

--- Program.cs
+++ Program.cs
@@ -1,3 +1,15 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+Console.WriteLine("Smartphone Nokia...");
+Smartphone nokia = new Nokia("13988888888", "Modelo 1", "123456789012345", 256);
+nokia.Ligar();
+nokia.ReceberLigacao();
+nokia.InstalarAplicativo("Spotify");
+
+Console.WriteLine("\n \n");
+
+Console.WriteLine("Smartphone iPhone...");
+Smartphone iphone = new Iphone("13977777777", "Modelo 17", "123456789012344", 512);
+iphone.Ligar();
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("Auto Destruição");
